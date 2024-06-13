841f8570f084ddfa79a1d38496b0658706bf4697
Nia Jordan <niakay.jo@gmail.com>
2024-06-13 14:12:22 -0500

    finished building Project class

--- src/main/model/Project.java
+++ src/main/model/Project.java
@@ -2,12 +2,35 @@ public class Project {
     
     private String name;
     private String date;
+    private String description;
+    private String criteria;
+    
 
-    setDueDate(){};
-    getDueDate(){};
-    setDescription(){};
-    getDescription(){};
-    setClientCriteria(){}
-    getClientCriteria(){};
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setDueDate(String d){
+        this.date = d;
+    }
+    public String getDueDate(){
+        return date;
+    }
+    public void setDescription(String descr){
+        this.description = descr;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setClientCriteria(String crit){
+        this.criteria = crit;
+    }
+    public String getClientCriteria(){
+        return criteria;
+    }
 
 }
