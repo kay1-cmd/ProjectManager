b6e41d28f107bef4af9278b5a3e6a680fa455269
annahickey1 <ahickey7@elon.edu>
2024-06-24 15:44:08 -0400

    Swagger Link

--- src/main/java/com/example/ProjectManager/Controller/freelanceController.java
+++ src/main/java/com/example/ProjectManager/Controller/freelanceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+//http://localhost:8080/swagger-ui/index.html
 
 @RestController
 @RequestMapping("/freelance")
